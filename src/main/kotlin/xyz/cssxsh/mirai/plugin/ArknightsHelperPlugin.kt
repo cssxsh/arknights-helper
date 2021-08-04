59b8d6a03dc7425736e40cc2946830244ccce7a4
cssxsh <cssxsh@gmail.com>
2021-08-04 18:27:34 +0800

    build: 1.2.3

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/ArknightsHelperPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/ArknightsHelperPlugin.kt
@@ -6,7 +6,7 @@ import net.mamoe.mirai.console.plugin.jvm.*
 import xyz.cssxsh.mirai.plugin.command.*
 
 object ArknightsHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.arknights-helper", "1.2.1") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.arknights-helper", "1.2.3") {
         name("arknights-helper")
         author("cssxsh")
     }
