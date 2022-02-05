1500a472f8a0908e3202ef7a9c8f00688ab04730
cssxsh <cssxsh@gmail.com>
2022-02-05 21:08:23 +0800

    build: 1.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.serialization") version "1.6.0"
 
-    id("net.mamoe.mirai-console") version "2.10.0-RC2"
+    id("net.mamoe.mirai-console") version "2.10.0"
     id("net.mamoe.maven-central-publish") version "0.7.1"
 }
 
@@ -20,24 +20,21 @@ mavenCentralPublish {
 
 repositories {
     mavenLocal()
-    maven(url = "https://maven.aliyun.com/repository/central")
     mavenCentral()
-    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
     gradlePluginPortal()
 }
 
 mirai {
-    jvmTarget = JavaVersion.VERSION_11
     configureShadow {
         exclude("module-info.class")
     }
 }
 
 dependencies {
-    compileOnly("net.mamoe:mirai-core:${mirai.coreVersion}")
-    compileOnly("net.mamoe:mirai-core-utils:${mirai.coreVersion}")
+    compileOnly("net.mamoe:mirai-core:2.10.0")
+    compileOnly("net.mamoe:mirai-core-utils:2.10.0")
 
-    testImplementation(kotlin("test", kotlin.coreLibrariesVersion))
+    testImplementation(kotlin("test", "1.6.0"))
 }
 
 tasks {
