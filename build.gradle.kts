ed85a08897c738ed8f4f461c389bbd773f71c123
cssxsh <cssxsh@gmail.com>
2022-05-23 23:32:51 +0800

    build: 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "xyz.cssxsh"
-version = "1.3.14"
+version = "1.4.0"
 
 mavenCentralPublish {
     useCentralS01()
--- src/main/kotlin/xyz/cssxsh/mirai/arknights/ArknightsHelperPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/arknights/ArknightsHelperPlugin.kt
@@ -10,7 +10,7 @@ import xyz.cssxsh.arknights.*
 import xyz.cssxsh.mirai.arknights.command.*
 
 object ArknightsHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.arknights-helper", "1.3.14") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.arknights-helper", "1.4.0") {
         name("arknights-helper")
         author("cssxsh")
     }
