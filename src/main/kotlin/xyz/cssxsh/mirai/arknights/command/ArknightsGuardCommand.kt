65de28ae280bb01455639af40155a9d227dd4a8c
cssxsh <cssxsh@gmail.com>
2022-09-05 23:19:16 +0800

    fix: ark-guard

--- src/main/kotlin/xyz/cssxsh/mirai/arknights/command/ArknightsGuardCommand.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/arknights/command/ArknightsGuardCommand.kt
@@ -1,50 +1,73 @@
 package xyz.cssxsh.mirai.arknights.command
 
 import net.mamoe.mirai.console.command.*
-import net.mamoe.mirai.contact.*
 import net.mamoe.mirai.message.data.*
+import xyz.cssxsh.arknights.announce.*
+import xyz.cssxsh.arknights.bilibili.*
+import xyz.cssxsh.arknights.weibo.*
 import xyz.cssxsh.mirai.arknights.*
+import xyz.cssxsh.mirai.arknights.data.*
 
 public object ArknightsGuardCommand : CompositeCommand(
     owner = ArknightsHelperPlugin,
-    "guard", "蹲饼",
+    "ark-guard", "方舟蹲饼",
     description = "明日方舟助手蹲饼指令"
 ) {
 
     @SubCommand("detail", "详情")
     @Description("查看蹲饼详情")
-    public suspend fun CommandSenderOnMessage<*>.detail(): Unit = reply {
-        buildMessageChain {
-            val list = GuardContacts.groupBy { it > 0 }
-            for (friend in list[true].orEmpty()) {
-                appendLine("friend: $friend")
+    public suspend fun CommandSender.detail() {
+        val message = buildMessageChain {
+            appendLine("=== 微博订阅 ===")
+            ArknightsTaskConfig.blog.forEach { (id, blogs) ->
+                append("$id : $blogs")
             }
-            for (group in list[false].orEmpty()) {
-                appendLine("group ${-group}")
+            appendLine("=== 视频订阅 ===")
+            ArknightsTaskConfig.video.forEach { (id, videos) ->
+                append("$id : $videos")
             }
-            appendLine("蹲饼间隔${GuardInterval}m")
+            appendLine("=== 公告订阅 ===")
+            ArknightsTaskConfig.announce.forEach { (id, announces) ->
+                append("$id : $announces")
+            }
         }
+
+        sendMessage(message = message)
     }
 
-    @SubCommand("speed", "速度")
-    @Description("设置微博蹲饼速度")
-    public suspend fun CommandSenderOnMessage<*>.speed(duration: Int): Unit = reply {
-        check(duration in 1..10) { "速度 不合法 1~10 分钟" }
-        GuardInterval = duration
-        "蹲饼速度已设置 ${duration}分钟".toPlainText()
+    @SubCommand("blog", "微博")
+    @Description("设置微博蹲饼内容")
+    public suspend fun CommandSender.blog(contact: Long, vararg blogs: BlogUser) {
+        ArknightsTaskConfig.blog[contact] = blogs.asList()
+        val message = buildMessageChain {
+            append("当前微博订阅内容 ")
+            append(blogs.joinToString(", ").ifEmpty { "为空" })
+        }
+
+        sendMessage(message = message)
     }
 
-    @SubCommand("open", "打开")
-    @Description("开启提醒")
-    public suspend fun CommandSenderOnMessage<*>.open(contact: Contact = fromEvent.subject): Unit = reply {
-        GuardContacts.add(contact.delegate)
-        "$contact 蹲饼已打开".toPlainText()
+    @SubCommand("video", "视频")
+    @Description("设置视频蹲饼内容")
+    public suspend fun CommandSender.video(contact: Long, vararg videos: VideoType) {
+        ArknightsTaskConfig.video[contact] = videos.asList()
+        val message = buildMessageChain {
+            append("当前视频订阅内容 ")
+            append(videos.joinToString(", ").ifEmpty { "为空" })
+        }
+
+        sendMessage(message = message)
     }
 
-    @SubCommand("close", "关闭")
-    @Description("关闭提醒")
-    public suspend fun CommandSenderOnMessage<*>.close(contact: Contact = fromEvent.subject): Unit = reply {
-        GuardContacts.remove(contact.delegate)
-        "$contact 蹲饼已关闭".toPlainText()
+    @SubCommand("announce", "公告")
+    @Description("设置视频蹲饼内容")
+    public suspend fun CommandSender.announce(contact: Long, vararg videos: AnnounceType) {
+        ArknightsTaskConfig.announce[contact] = videos.asList()
+        val message = buildMessageChain {
+            append("当前公告订阅内容 ")
+            append(videos.joinToString(", ").ifEmpty { "为空" })
+        }
+
+        sendMessage(message = message)
     }
 }
