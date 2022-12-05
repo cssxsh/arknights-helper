2f225a727f751be4de9ca592d58b4a3111b55e7e
cssxsh <cssxsh@gmail.com>
2022-12-05 08:42:35 +0800

    update: mirai 2.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.serialization") version "1.7.21"
 
-    id("net.mamoe.mirai-console") version "2.13.1"
+    id("net.mamoe.mirai-console") version "2.13.2"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -32,7 +32,7 @@ dependencies {
     compileOnly("xyz.cssxsh:meme-helper:1.1.5")
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.13.1"))
+    implementation(platform("net.mamoe:mirai-bom:2.13.2"))
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
