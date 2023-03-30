6726ad38823636036351f5a780993bd23dddfc5b
cssxsh <cssxsh@gmail.com>
2023-03-30 20:00:07 +0800

    update: UserAgent, for #38

--- src/main/kotlin/xyz/cssxsh/arknights/CacheDataHolder.kt
+++ src/main/kotlin/xyz/cssxsh/arknights/CacheDataHolder.kt
@@ -16,7 +16,9 @@ import java.io.*
 public abstract class CacheDataHolder<K : CacheKey, R : CacheInfo> {
     protected open val mutex: Mutex = Mutex()
     protected open val http: HttpClient = HttpClient(OkHttp) {
-        BrowserUserAgent()
+        install(UserAgent) {
+            agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        }
         ContentEncoding()
         expectSuccess = true
         install(HttpTimeout) {
